4c20c9d9ee1dff839729d283c817dc70fac01787
Markovkin Aleksandr <markovkin88@gmail.com>
2019-06-07 10:36:32 +0300

    Вводит поддержку localStorage

--- js/script.js
+++ js/script.js
@@ -1,16 +1,57 @@
 // Раздел переменных
-var modalwrite = document.querySelector(".modal-writeus");
+var modalWrite = document.querySelector(".modal-writeus");
 var modal = document.querySelector(".modal");
 var close = modal.querySelector(".modal-close");
+var form = modal.querySelector("form");
+var modalName = modal.querySelector("[name=name]");
+var email = modal.querySelector("[name=email]");
+var emailText = modal.querySelector("[name=email-text]");
+
+var isStorageSupport = true;
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 // Раздел открытия формы
-modalwrite.addEventListener("click", function (evt) {
+modalWrite.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal.classList.add("modal-show");
+
+  if (storage) {
+    modalName.value = localStorage.getItem("modalName");
+    email.value = localStorage.getItem("email");
+    emailText.focus();
+  } else {
+    modalName.focus();
+  }
 });
 
 // Раздел закрытия формы
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!modalName.value || !email.value || !emailText.value) {
+    evt.preventDefault();
+    console.log("Нужно ввести имя, адрес почты и текст сообщения");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("modalName", modalName.value);
+      localStorage.setItem("email", email.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modal.classList.contains("modal-show")) {
+      modal.classList.remove("modal-show");
+    }
+  }
 });
